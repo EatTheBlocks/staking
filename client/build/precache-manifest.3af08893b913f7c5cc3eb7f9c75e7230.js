10adc1054777400353e6248d0538182318d55293
Julien Klepatch <julien@julienklepatch.com>
2021-10-04 15:02:23 +0800

    build frontend

---
+++ client/build/precache-manifest.3af08893b913f7c5cc3eb7f9c75e7230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50af53568e9ce282cd25ce5a67b73397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fbaf2a2fea72bdcfacd",
+    "url": "/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "592b7e0177b47459785b",
+    "url": "/static/css/main.c7814b07.chunk.css"
+  },
+  {
+    "revision": "5fbaf2a2fea72bdcfacd",
+    "url": "/static/js/2.48a589c8.chunk.js"
+  },
+  {
+    "revision": "592b7e0177b47459785b",
+    "url": "/static/js/main.cf5fc6cd.chunk.js"
+  },
+  {
+    "revision": "5b528a6b397ab46ad443",
+    "url": "/static/js/runtime-main.1fef1943.js"
+  },
+  {
+    "revision": "8c4db20c33880d267dce22f5d610d23d",
+    "url": "/static/media/logo.8c4db20c.png"
+  }
+]);
